32f0d577cba35cd4d928b6fa65914ed3e92e9f36
Mihail Marian <m.marian@elsevier.com>
2024-04-12 14:44:41 +0100

    responding to post request with openai

---
+++ src/lib/types.d.ts
@@ -0,0 +1,1 @@
+export type CompletionRequest = { message: string };
--- src/pages/api/ai.ts
+++ src/pages/api/ai.ts
@@ -1,19 +1,21 @@
+import type { CompletionRequest } from "../../lib/types";
+
 import type { APIRoute } from "astro";
 import OpenAI from "openai";
 
 export const POST: APIRoute = async ({ request }) => {
   const openai = new OpenAI({ apiKey: import.meta.env.OPENAI_API_KEY });
 
+  const body = (await request.json()) as CompletionRequest;
+
   const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "You are a helpful assistant." }],
+    messages: [{ role: "system", content: body.message }],
     model: "gpt-3.5-turbo",
   });
 
-  console.log(completion.choices[0]);
-
   return new Response(
     JSON.stringify({
-      message: import.meta.env.OPENAI_API_KEY,
+      message: completion.choices[0],
     }),
     { status: 200 }
   );
