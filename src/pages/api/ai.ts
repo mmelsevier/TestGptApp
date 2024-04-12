3cc6efa98a264e834bb607f3c66ed59363b9828f
Mihail Marian <m.marian@elsevier.com>
2024-04-12 13:57:40 +0100

    integrated with openai

--- src/pages/api/ai.ts
+++ src/pages/api/ai.ts
@@ -1,9 +1,19 @@
 import type { APIRoute } from "astro";
+import OpenAI from "openai";
 
 export const POST: APIRoute = async ({ request }) => {
+  const openai = new OpenAI({ apiKey: import.meta.env.OPENAI_API_KEY });
+
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: "system", content: "You are a helpful assistant." }],
+    model: "gpt-3.5-turbo",
+  });
+
+  console.log(completion.choices[0]);
+
   return new Response(
     JSON.stringify({
-      message: "Success!",
+      message: import.meta.env.OPENAI_API_KEY,
     }),
     { status: 200 }
   );
