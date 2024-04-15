8ff132fe7ac3fd0b5ad094c1c794c8cdda5232c9
Mihail Marian <m.marian@elsevier.com>
2024-04-15 19:28:43 +0100

    added tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 import node from "@astrojs/node";
-
 import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,5 +10,5 @@ export default defineConfig({
   adapter: node({
     mode: "standalone"
   }),
-  integrations: [react()]
+  integrations: [react(), tailwind()]
 });
