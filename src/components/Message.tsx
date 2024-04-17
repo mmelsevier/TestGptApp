ff03fe407064140cafef8b035c90b6d00ebdf777
Mihail Marian <m.marian@elsevier.com>
2024-04-17 17:16:21 +0100

    sorted markdown

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,3 +1,7 @@
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { darcula } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import Markdown from "react-markdown";
+
 import type { MessagePayload } from "../lib/types";
 
 export default function Message({ message }: { message: MessagePayload }) {
@@ -9,7 +13,37 @@ export default function Message({ message }: { message: MessagePayload }) {
           : "bg-purple-950 text-gray-100"
       } p-3 my-2 rounded-md`}
     >
-      {message.content}
+      {message.role == "user" ? (
+        message.content
+      ) : (
+        <Markdown
+          components={{
+            code: CodeMarkdownReplacement,
+          }}
+        >
+          {message.content}
+        </Markdown>
+      )}
     </div>
   );
 }
+
+type CodeMarkdownReplacementProps = {
+  children?: React.ReactNode;
+  className?: string | undefined;
+};
+
+const CodeMarkdownReplacement = (props: CodeMarkdownReplacementProps) => {
+  const match = /language-(\w+)/.exec(props.className || "");
+  return match ? (
+    <SyntaxHighlighter
+      children={String(props.children).replace(/\n$/, "")}
+      language={match[1]}
+      style={darcula}
+    />
+  ) : (
+    <code className="bg-gray-700 text-orange-400 font-mono text-sm px-[4px] py-[1px] rounded-md">
+      {props.children}
+    </code>
+  );
+};
